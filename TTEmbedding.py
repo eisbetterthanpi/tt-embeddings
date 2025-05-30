b038f6d276dd527341aff4530c2047ecfba1bef1
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2025-05-30 18:56:09 +0800

    Update TTEmbedding.py

--- TTEmbedding.py
+++ TTEmbedding.py
@@ -38,7 +38,7 @@ import math
 class TTEmbedding(nn.Module):
     def __init__(self, in_dim, d_model, rank=256):
         super().__init__()
-        self.ttlin = TTLinear(in_dim, d_model)
+        self.ttlin = TTLinear(in_dim, d_model, rank, std=1) # https://docs.pytorch.org/docs/stable/generated/torch.nn.Embedding.html
         self.weight = self.ttlin.weight
         self.num_classes = math.prod(in_dim)
 
@@ -71,4 +71,3 @@ class TTEmbedding(nn.Module):
 # # plt.hist(x.flatten().tolist(), bins=20, alpha=.5, label='context mask')
 # plt.hist([o[:100,:100].flatten().tolist(), x.flatten().tolist()], bins=20, alpha=.5, label='context mask')
 # plt.show()
-
